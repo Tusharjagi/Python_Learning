9a2861d4268c901533519a927eba4696a8da2713
Tusharjagi <tusharjagi@gmail.com>
2022-02-26 15:55:14 +0530

    Update

--- Basics_of_python.py
+++ Basics_of_python.py
@@ -13,3 +13,20 @@
 # 8. Databases
 # 9. GUI Programming
 # 10. Scalable
+
+# ------------> Data Structure in Python <---------------------------
+# -------> Build In <--------
+# 1. List                 -> My_List = []               -> Mutable
+# 2. Dictionary           -> My_Dict = {}               -> Mutable
+# 3. Tuple                -> My_tuple = ()              -> Immutable
+# 4. Sets                 -> My_Set = {}                -> Mutable
+
+# -------> User Define <--------
+# 1. Arrays
+# 2. Stack
+# 3. Queue
+# 4. Linked Lists
+# 5. Tree
+# 6. Graphs
+# 7. Hash Table
+
