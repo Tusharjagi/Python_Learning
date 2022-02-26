5418a673b46c8c744f702cbc3b7bea0be8870d6e
Tusharjagi <tusharjagi@gmail.com>
2022-02-26 15:12:50 +0530

    Update

---
+++ Basics_of_python.py
@@ -0,0 +1,15 @@
+# 1. Python is interpreted language - means line by line.
+# 2. Python is Interactive - means interact with prompt.
+# 3. Python is Object-Oriented.
+
+# Feature of Python
+# 1. Easy-to-learn
+# 2. East-to-learn
+# 3. Easy-to-maintain
+# 4. Broad standard Library
+# 5. Interactive Mode
+# 6. Portable
+# 7. Extendable
+# 8. Databases
+# 9. GUI Programming
+# 10. Scalable
