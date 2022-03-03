4ea693467c23033b736317b1f157cdd96c07c23f
Tusharjagi <tusharjagi@gmail.com>
2022-03-03 19:17:29 +0530

    Update

---
+++ Loops/FOR.py
@@ -0,0 +1,12 @@
+# for iterationg_var in sequence:
+#     statements(s)
+
+for key in range(5):
+    print(key)
+
+for letter in 'python':
+    print('Letter in Python', letter)
+
+fruits = ['apple', 'kivy', 'pineapple', 'orange']
+for fruit in fruits:
+    print('fruits list : ', fruit)
---
+++ Loops/While.py
@@ -0,0 +1,17 @@
+# while expression:
+#     statements(s)
+
+count = 0
+while count <= 10:
+    print("This is count is :", count)
+    count += 1
+
+print("Good Bye !!!! ")
+
+# Infinite Loop
+var = 1
+while var == 1:
+    num = int(input("Enter a number : "))
+    print("You entered: ", num)
+print("Good bye!!!!!! ")
+
